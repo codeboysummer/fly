91d836c448840089b3eb6385c4fa90bb4bb1b003
codeboysummer <aw.wahid13@gmail.com>
2023-01-19 11:48:59 -0500

    ' ui animation and formik and yup validation'

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -10,14 +10,42 @@ import {
   Input,
   Link,
   useToast,
+  FormControl,
+  Collapse,
 } from "@chakra-ui/react";
 import { signInWithPopup } from "firebase/auth";
 import { GoogleAuthProvider } from "firebase/auth";
 import { Image } from "@chakra-ui/react";
 import { auth, googleAuthProvider } from "../lib/firebase";
-import {} from "react-firebase-hooks/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useFormAnimation, useMyAnimation } from "../lib/animations";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import { Formik, Field } from "formik";
+import * as yup from "yup";
+import { signOut } from "firebase/auth";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 const Login = () => {
+  const [user] = useAuthState(auth);
+
+  const { animation, startAnimation } = useMyAnimation();
+
+  const [showLogin, setshowLogin] = useState(false);
+
+  const { formAnimation, startFormAnimation } = useFormAnimation();
+
   const toast = useToast();
+
+  const validationSchema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().min(8).required(),
+  });
+
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
   const signInWithGoogle = async () => {
     try {
       await signInWithPopup(auth, googleAuthProvider);
@@ -25,21 +53,190 @@ const Login = () => {
     } catch (error) {}
   };
 
+  const CreateAccountWithCredentials = async (
+    email: String,
+    password: String
+  ) => {
+    try {
+      createUserWithEmailAndPassword(auth, email, password);
+      toast({ title: "welcome !", status: "success", duration: 1000 });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const runAnimations = async () => {
+    if (user) {
+      await startFormAnimation();
+      startAnimation();
+    }
+  };
+
+  function Login() {
+    return (
+      <>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={(values, { setSubmitting }) => {
+            // handle form submit here
+            setSubmitting(false);
+          }}
+        >
+          {({ errors, touched, isSubmitting }) => (
+            <FormControl height={"40vh"}>
+              <VStack h={"100%"} gap={"10%"}>
+                <Input
+                  w={"50%"}
+                  name="email"
+                  placeholder="Email"
+                  isInvalid={!!(errors.email && touched.email)}
+                  aria-invalid={!!(errors.email && touched.email)}
+                  aria-describedby={`error-email`}
+                  id={`email`}
+                  type="email"
+                />
+                {errors.email && touched.email ? (
+                  <div id={`error-email`}>{errors.email}</div>
+                ) : null}
+                <Input
+                  name="password"
+                  w={"50%"}
+                  placeholder="Password"
+                  isInvalid={!!(errors.password && touched.password)}
+                  aria-invalid={!!(errors.password && touched.password)}
+                  aria-describedby={`error-password`}
+                  id={`password`}
+                  type="password"
+                />
+                {errors.password && touched.password ? (
+                  <div id={`error-password`}>{errors.password}</div>
+                ) : null}
+                <Button
+                  colorScheme={"blue"}
+                  w={"50%"}
+                  onClick={() => {}}
+                  disabled={isSubmitting}
+                >
+                  login
+                </Button>
+                <Text>
+                  have an account ?{" "}
+                  <Link
+                    onClick={() => setshowLogin(!showLogin)}
+                    fontWeight={"bold"}
+                  >
+                    login here!
+                  </Link>
+                </Text>
+                <Button onClick={() => signOut(auth)}>singout</Button>
+              </VStack>
+            </FormControl>
+          )}
+        </Formik>
+      </>
+    );
+  }
+
+  function Register() {
+    return (
+      <>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={async (values, { setSubmitting }) => {
+            const { email, password } = values;
+
+           await CreateAccountWithCredentials(email, password);
+            // handle form submit here
+
+            setSubmitting(false);
+          }}
+        >
+          {({ errors, touched, isSubmitting }) => (
+            <FormControl height={"40vh"}>
+              <VStack h={"100%"} gap={"10%"}>
+                <Input
+                  w={"50%"}
+                  name="email"
+                  placeholder="Email"
+                  isInvalid={!!(errors.email && touched.email)}
+                  aria-invalid={!!(errors.email && touched.email)}
+                  aria-describedby={`error-email`}
+                  id={`email`}
+                  type="email"
+                />
+                {errors.email && touched.email ? (
+                  <div id={`error-email`}>{errors.email}</div>
+                ) : null}
+                <Input
+                  name="password"
+                  w={"50%"}
+                  placeholder="Password"
+                  isInvalid={!!(errors.password && touched.password)}
+                  aria-invalid={!!(errors.password && touched.password)}
+                  aria-describedby={`error-password`}
+                  id={`password`}
+                  type="password"
+                />
+                {errors.password && touched.password ? (
+                  <div id={`error-password`}>{errors.password}</div>
+                ) : null}
+                <Button
+                  colorScheme={"blue"}
+                  w={"50%"}
+                  type="submit"
+                  disabled={isSubmitting}
+                >
+                  Register
+                </Button>
+
+                <Text>
+                  dont have an account ?{" "}
+                  <Link
+                    onClick={() => setshowLogin(!showLogin)}
+                    fontWeight={"bold"}
+                  >
+                    register for free!
+                  </Link>
+                </Text>
+                <Button onClick={() => signOut(auth)}>singout</Button>
+              </VStack>
+            </FormControl>
+          )}
+        </Formik>
+      </>
+    );
+  }
+
+  useEffect(() => {
+    runAnimations();
+  }, [user]);
+
   return (
     <>
       {" "}
       <HStack justifyContent={"space-between"} w={"100%"} h={"100vh"}>
         <Image
+          objectFit={"cover"}
+          as={motion.img}
           alt=""
           borderRightRadius={"40%"}
           w={"50vw"}
           h={"100vh"}
-          src={
-            "https://wallpapershome.com/images/wallpapers/fuji-3840x2160-4k-hd-wallpaper-japan-travel-tourism-national-10326.jpg"
-          }
+          animate={animation}
+          initial={{ width: "50vw" }}
+          src={"/fugi.jpg"}
         />
 
-        <VStack gap={"1rem"} pt={"10%"} w={"50vw"} h={"100vh"}>
+        <VStack
+          animate={formAnimation}
+          as={motion.div}
+          gap={"1rem"}
+          pt={"10%"}
+          w={"50vw"}
+          h={"100vh"}
+        >
           <Heading>Welcome to Fly</Heading>
           <Text color={"darkgrey"}>
             Continue with Google or Enter your details
@@ -60,25 +257,8 @@ const Login = () => {
             login with google
           </Button>
           <Text>or</Text>
-          <Input placeholder="Email" w={"50%"} />
-          <Input placeholder="Password" w={"50%"} />
-          <Button
-            _hover={{
-              background: "#77aae5",
-              color: "white",
-            }}
-            bg={"hotpink"}
-            color={"white"}
-            w={"50%"}
-          >
-            Login
-          </Button>
-          <>
-            <Text>
-              Don&apos;t have an account?{" "}
-              <Link fontWeight={"bold"}>Sign up For free</Link>
-            </Text>
-          </>
+
+          {showLogin ? <Login /> : <Register />}
         </VStack>
       </HStack>
     </>
